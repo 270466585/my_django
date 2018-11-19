83d2d1e7365a4960659fb10d5ebba61cea71efab
ZhaoQiao <270466585@qq.com>
2018-11-19 23:41:48 +0800

    the next day

--- myAPP/urls.py
+++ myAPP/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.index,name='index'),
+    url(r'^(?P<question_id>[0-9]+)/$',views.detail,name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$',views.results,name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$',views.vote,name='vote'),
+    url(r'^(?P<username>[A-Za-z0-9]+)/createUser',views.createUser,name='createUser'),
 ]
--- myAPP/views.py
+++ myAPP/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 # Create your views here.
 
 #创建第一个视图
+# def index(request):
+#     return HttpResponse("hello ,world .you're at the polls index" )
 def index(request):
-    return HttpResponse("hello ,world .you're at the polls index" )
+    latest_question_list=Question.objects.order_by('-pub_date')[:5]
+    template=loader.get_template('myAPP/index.html')
+    context={
+        'latest_question_list':latest_question_list,
+    }
+    return HttpResponse(template.render(context,request))
+
+def detail(request,question_id):
+    return HttpResponse("You'er looking at question %s."%question_id)
+
+def results(request,question_id):
+    response="You're looking at the results of question %s."
+    return HttpResponse(response%question_id)
+
+def vote(request,question_id):
+    return HttpResponse("Youe're voting on question %s."%question_id)
+
+def createUser(request,username):
+    return HttpResponse("Create a user with the account name:%s"%username)
